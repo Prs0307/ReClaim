91b7f1c238fef0cb885ffb772664206a9866fb26
thedinkardubey <sde.dinkardubey@gmail.com>
2024-09-08 17:18:08 +0530

    Added Routes only changed app.py

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
+from flask import redirect, render_template, request, url_for
 from __init__ import create_app
 
 # Create an application instance
 app = create_app()
+
+# By Dinkar
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    return render_template('result.html')
+@app.route('/address', methods=['GET', 'POST'])
+def address():
+    return render_template('user_detail.html')
+@app.route('/submit-details', methods=['GET', 'POST'])
+def submitdetails():
+    return render_template('nearby_shop.html')
 
 # Run the application
 if __name__ == '__main__':
